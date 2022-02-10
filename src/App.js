9dcd197e21b3828a9375bb2178016d43f076cb13
parvathi15 <menonparvathi23@gmail.com>
2022-02-10 17:23:09 +0100

    vnbvbn

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import ViewBook from "./Components/user/ViewBook";
 import AddBook from "./Components/admin/AddBook";
 import EditBook from "./Components/admin/EditBook";
 import Adminpage from "./Components/admin/Adminpage";
-// import Bookrequests from "./Components/admin/Bookrequests";
+import Bookrequests from "./Components/admin/Bookrequests";
 import RequestList from "./Components/admin/RequestList";
 import Register from "./Components/admin/Register";
 import BookList from "./Components/admin/BookList";
-// import EditRequest from "./Components/admin/edit-request";
-// import EditBookReq from "./Components/admin/EditBookReq";
-// import ImpFine from "./Components/admin/ImpFine";
-// import BookRecords from "./Components/admin/Returnbooks/BookRecords";
-// import EditBookReturn from "./Components/admin/Returnbooks/EditBookReturn";
+import EditRequest from "./Components/admin/edit-request";
+import EditBookReq from "./Components/admin/EditBookReq";
+import ImpFine from "./Components/admin/ImpFine";
+import BookRecords from "./Components/admin/Returnbooks/BookRecords";
+import EditBookReturn from "./Components/admin/Returnbooks/EditBookReturn";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -48,15 +48,13 @@ export default class App extends Component {
     <Route path="/addbook" component={AddBook} />
     <Route path="/list" component={BookList} />
     <Route path="/editbook/:id" component={EditBook} />
-    {/* <Route path="/records" component={Bookrequests} /> */}
+    <Route path="/records" component={Bookrequests} /> 
     <Route path="/userreqs" component={RequestList} />
-    {/* 
-    <Route path="/records" component={Bookrequests} />
     <Route path="/edituser/:id" component={EditRequest} />
     <Route path="/editbkre/:id" component={EditBookReq} />
      <Route path="/editreturn/:id" component={EditBookReturn} />
     <Route path="/fine/:id" component={ImpFine} />
-    <Route path="/return" component={BookRecords} /> */}
+    <Route path="/return" component={BookRecords} /> 
    
 
     </div>
