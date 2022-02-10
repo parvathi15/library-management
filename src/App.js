0d22d5317616f46ab495f7f08712662d8f0da3e4
parvathi15 <menonparvathi23@gmail.com>
2022-02-10 12:09:03 +0100

    bhabha

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ import Userbooks from "./Components/user/Userbooks";
 import UserProfile from "./Components/user/UserProfile";
 import BookHistory from "./Components/user/BookHistory";
 import ViewBook from "./Components/user/ViewBook";
-import AddBook from "./Components/admin/AddBook";
-import EditBook from "./Components/admin/EditBook";
-import Adminpage from "./Components/admin/Adminpage";
-import Bookrequests from "./Components/admin/Bookrequests";
-import RequestList from "./Components/admin/RequestList";
-import Register from "./Components/admin/Register";
-import EditRequest from "./Components/admin/edit-request";
-import EditBookReq from "./Components/admin/EditBookReq";
-import BookList from "./Components/admin/BookList";
-import ImpFine from "./Components/admin/ImpFine";
-import BookRecords from "./Components/admin/Returnbooks/BookRecords";
-import EditBookReturn from "./Components/admin/Returnbooks/EditBookReturn";
+// import AddBook from "./Components/admin/AddBook";
+// import EditBook from "./Components/admin/EditBook";
+// import Adminpage from "./Components/admin/Adminpage";
+// import Bookrequests from "./Components/admin/Bookrequests";
+// import RequestList from "./Components/admin/RequestList";
+// import Register from "./Components/admin/Register";
+// import EditRequest from "./Components/admin/edit-request";
+// import EditBookReq from "./Components/admin/EditBookReq";
+// import BookList from "./Components/admin/BookList";
+// import ImpFine from "./Components/admin/ImpFine";
+// import BookRecords from "./Components/admin/Returnbooks/BookRecords";
+// import EditBookReturn from "./Components/admin/Returnbooks/EditBookReturn";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -38,22 +38,24 @@ export default class App extends Component {
   <Route exact path="/Register" component={Register} />
   <div>
     <Navbar />
-    <Route path="/admin" component={Adminpage} />
+    <Route path="/homepage" component={HomePage} />
+    <Route path="/profile" component={UserProfile} />
+    <Route path="/history" component={BookHistory} /> 
+    <Route path="/userbook" component={Userbooks} />
+    <Route path="/viewbook/:id" component={ViewBook} />
+    {/* <Route path="/admin" component={Adminpage} />
     <Route path="/userreqs" component={RequestList} />
     <Route path="/records" component={Bookrequests} />
     <Route path="/edituser/:id" component={EditRequest} />
     <Route path="/editbkre/:id" component={EditBookReq} />
-    <Route path="/homepage" component={HomePage} />
-    <Route path="/profile" component={UserProfile} />
     <Route path="/addbook" component={AddBook} />
     <Route path="/editbook/:id" component={EditBook} />
     <Route path="/list" component={BookList} />
-    <Route path="/history" component={BookHistory} /> 
-    <Route path="/editreturn/:id" component={EditBookReturn} />
+     <Route path="/editreturn/:id" component={EditBookReturn} />
     <Route path="/fine/:id" component={ImpFine} />
     <Route path="/return" component={BookRecords} />
-    <Route path="/userbook" component={Userbooks} />
-    <Route path="/viewbook/:id" component={ViewBook} />
+    */}
+
     </div>
     </Switch>
   </Router>
